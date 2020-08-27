de4ffd3f6c3ba38bb338207e83a9470d42b17091
Amit-Pallauri-au6 <apallauri@gmail.com>
2020-08-27 14:52:51 +0530

    handled error

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const { config } = require('dotenv')
-const passport = require('passport')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 config()
 require('./db')
-require('./utils/passport')
 require("./utils/razorpay")
 
 const app = express()
@@ -21,16 +19,13 @@ app.use(
   //     })
 )
 // app.options('*', cors())
-app.use(passport.initialize())
 
 const userRoutes = require('./routes/userRoutes')
 const apiRoutes = require('./routes/apiRoutes');
 const adminRoutes = require("./routes/adminRoute");
-const chatRoutes = require('./routes/chatRoutes')
 app.use(userRoutes)
 app.use(apiRoutes)
 app.use(adminRoutes)
-app.use(chatRoutes)
 
 
 module.exports  = app
--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const { getChats }  = require('../controllers/chatController')
-
-router.get('/getChats', getChats)
-
-module.exports = router
